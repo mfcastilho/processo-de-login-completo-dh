4b7c14793ca797ed1a4e00223dd66d4932608fe9
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-02-02 01:44:50 -0300

    Creating all the methods we are going to use in the UsersModel.js file

--- src/models/UsersModel.js
+++ src/models/UsersModel.js
@@ -1,11 +1,61 @@
 const path = require("path");
 const fs = require("fs");
 const database = require("../database/database.json");
+const { mainModule } = require("process");
+const pathDatabase = path.resolve("src", "database", "databse.json");
 
 
 const UsersModel = {
   findAll:()=>{
-
     return database.users;
+  },
+
+  findUserById:(userId)=>{
+    const users = database.users;
+
+    const userFound = users.find(user => user.id == userId);
+
+    return userFound;
+  },
+  findUserByEmail:(userEmail)=>{
+    const users = database.users;
+
+    const userFound = users.find(user => user.email== userEmail);
+
+    return userFound;
+  },
+  findUserByFields:(field, value)=>{
+    const users = database.users;
+
+    const userFound = users.find(user => user[field]== value);
+
+    return userFound;
+  },
+  generateId:()=>{
+
+    const users = database.users;
+
+    let lastUser =users.pop();
+
+    if(lastUser){
+      return lastUser.id + 1;
+    }
+
+    return 1;
+
+  },
+  create:(userData)=>{const users = database.users;
+    const newUser = {
+      id:this.generateId(),
+      ...userData
+    }
+
+    database.users.push(newUser);
+    const dbJSON = JSON.stringify(database);
+    fs.writeFileSync(pathDatabase, dbJSON);
   }
 }
+
+console.log(UsersModel.findUserByFields("email", "mario@mail.com"));
+
+
